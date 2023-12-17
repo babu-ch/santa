8a343d30a6b41c63f9534e6fe2ca645c56c5857e
babu-ch <mashirohira@gmail.com>
2023-12-17 11:37:50 +0900

    refactor

--- src/scenes/MyScene.ts
+++ src/scenes/MyScene.ts
@@ -10,17 +10,28 @@ const DROP_PRESENT_PROBABILITY = 20
 const DROP_TONAKAI_PROBABILITY = 6
 const SANTA_SIZE = 100
 const ITEM_SIZE = 100
+// このmsの周期でさんたが落とすイベントを実行
 const DROP_ITEM_DELAY = 200
 const HISTORY_MAX_SIZE = 10
+// プレゼント取ったときのスコア
+const PRESENT_SCORE = 10
+// トナカイ落としたときのスコア
+const TONAKAI_SCORE = 10
 
 export default class MyScene extends Phaser.Scene {
+    // サンタさん
     private santa?: Image
+    // サンタの動く方向とスピード
     private santaVelocityX: number = SANTA_MOVE_SPEED
     private isGameOver: boolean = false
     private score: number = 0
+    // プレゼント落とすイベント
     private dropPresentEvent?: Phaser.Time.TimerEvent
+    // プレゼント落とす確率
     private dropPresentProbability: number = DROP_PRESENT_PROBABILITY
+    // トナカイ落とす確率
     private dropTonakaiProbability: number = DROP_TONAKAI_PROBABILITY
+    // 直近の落としたか落としてないか
     private recentDropHistories: ("drop"|"nodrop")[] = []
 
     preload() {
@@ -45,7 +56,7 @@ export default class MyScene extends Phaser.Scene {
                 .setDisplaySize(SANTA_SIZE, SANTA_SIZE)
                 .setStatic(true)
 
-        //
+        // プレゼント落とすイベント開始
         this.dropPresentEvent = this.time.addEvent({
             delay: DROP_ITEM_DELAY,
             callback: this.dropPresent,
@@ -53,6 +64,7 @@ export default class MyScene extends Phaser.Scene {
             loop: true
         });
 
+        // 当たり判定
         this.setCollision()
     }
 
@@ -60,17 +72,19 @@ export default class MyScene extends Phaser.Scene {
         if (this.isGameOver) {
             return
         }
-        const halfSanta = this.santa!.displayWidth/2;
-        if (this.santa!.x >= (this.sys.canvas.width - (halfSanta))) {
+        //  サンタがどっちに動くか
+        const santa = this.santa!
+        const halfSanta = santa.displayWidth/2;
+        if (santa.x >= (this.sys.canvas.width - (halfSanta))) {
             // 左に
             this.santaVelocityX = -SANTA_MOVE_SPEED
-            this.santa!.setFlipX(false)
-        } else if(this.santa!.x <= (halfSanta)) {
+            santa.setFlipX(false)
+        } else if(santa.x <= (halfSanta)) {
             // 右に
             this.santaVelocityX = SANTA_MOVE_SPEED
-            this.santa!.setFlipX(true)
+            santa.setFlipX(true)
         }
-        this.santa!.x += this.santaVelocityX
+        santa.x += this.santaVelocityX
     }
 
     setCollision() {
@@ -86,7 +100,7 @@ export default class MyScene extends Phaser.Scene {
                 if (isDropItem(o1) && isDropItem(o2)) {
                     o1.gameObject?.setVelocityX(-2)
                     o2.gameObject?.setVelocityX(2)
-                    // プレゼント同士衝突してスマッシュされるのでぶつかったときは上にあげてあげる
+                    // プレゼント同士衝突してスマッシュされるのでぶつかったときは上にあげてあげる(やさしさ)
                     o1.gameObject?.setVelocityY(-5)
                     o2.gameObject?.setVelocityY(-5)
                     return
@@ -99,13 +113,16 @@ export default class MyScene extends Phaser.Scene {
                 }
                 if (isTonakai(o1, o2) && isWorldWall && isOnGround(this.sys.canvas.height, o1, o2)) {
                     isTonakai(o1) ? o1.gameObject.destroy() : o2.gameObject.destroy()
-                    this.addScore(10)
+                    this.addScore(TONAKAI_SCORE)
                     return
                 }
             })
         });
     }
 
+    /**
+     * 落とすかどうかを判定する
+     */
     shouldDrop() {
         const isDrop =
             this.recentDropHistories.filter(history => history === "nodrop").length === HISTORY_MAX_SIZE
@@ -130,9 +147,10 @@ export default class MyScene extends Phaser.Scene {
 
         // 50%で画像を反転するか決める
         const isInvertImage = lottery(1);
+        const santa = this.santa!
         // たまにトナカイ落とす。
         if (lottery(this.dropTonakaiProbability)) {
-            const tonakai = this.matter.add.image( this.santa!.x, this.santa!.y+this.santa!.displayHeight, "tonakai")
+            const tonakai = this.matter.add.image( santa.x, santa.y+santa.displayHeight, "tonakai")
                 .setDisplaySize(ITEM_SIZE, ITEM_SIZE)
                 .setInteractive()
                 .setVelocityX(random(-2, 2))
@@ -140,6 +158,7 @@ export default class MyScene extends Phaser.Scene {
             if (isInvertImage) {
                 tonakai.setFlipX(true)
             }
+            // トナカイは拾ったらゲームオーバー
             tonakai.on("pointerdown", () => {
                 tonakai.destroy()
                 this.gameOver()
@@ -147,7 +166,7 @@ export default class MyScene extends Phaser.Scene {
             return;
         }
         // プレゼント落とす。
-        const present = this.matter.add.image(  this.santa!.x, this.santa!.y+this.santa!.displayHeight,"present")
+        const present = this.matter.add.image(  santa.x, santa.y+santa.displayHeight,"present")
             .setDisplaySize(ITEM_SIZE, ITEM_SIZE)
             .setInteractive()
             .setVelocityX(random(-2, 2))
@@ -155,9 +174,10 @@ export default class MyScene extends Phaser.Scene {
         if (isInvertImage) {
             present.setFlipX(true)
         }
+        // プレゼントは拾ったらスコア加算
         present.on("pointerdown", () => {
             present.destroy()
-            this.addScore(10)
+            this.addScore(PRESENT_SCORE)
         })
     }
 
@@ -203,20 +223,25 @@ export default class MyScene extends Phaser.Scene {
     }
 }
 
+// bodiesのどれかがワールド境界か？
+// 他に判断できる方法がわからなかったのでgameObjectがなければ壁ってことにしています
 function isWall(...bodies:BodyType[]) {
     return !!bodies.find(body => !body?.gameObject)
 }
 
+// bodiesのどれかがプレゼントかトナカイか
 function isDropItem(...bodies: BodyType[]) {
     return isPresent(...bodies) || isTonakai(...bodies)
 }
+// bodiesのどれかがプレゼントか
 function isPresent(...bodies: BodyType[]) {
     return !!bodies.find(body => body?.gameObject?.texture?.key === "present")
 }
+// bodiesのどれかがトナカイか
 function isTonakai(...bodies: BodyType[]) {
     return !!bodies.find(body => body?.gameObject?.texture?.key === "tonakai")
 }
-
+// bodiesのどれかが地面に当たったか
 function isOnGround(worldHeight:number, ...bodies: BodyType[]) {
     const item = bodies.find(body => body?.gameObject)
     if (!item) {
