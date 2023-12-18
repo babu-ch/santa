b3e78155aefbeaa3c5b21669e1b7a919b88abda2
babu-ch <mashirohira@gmail.com>
2023-12-18 15:47:31 +0900

    grav

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ const config: Phaser.Types.Core.GameConfig = {
         default: "matter",
         matter: {
             // debug: true,
-            gravity: { y: isPc ? 0.2 : 1.2 },
+            gravity: { y: isPc ? 0.2 : 1.2 }, // PCとスマホ実機で落下速度が違ったのでとりあえず調整 なぜなのか？
             enableSleeping: true
         },
     },
